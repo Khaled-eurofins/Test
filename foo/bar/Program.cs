48deca573a6ba5c323450fe57cd7a578d28a97a0
Khaled-eurofins <KhaledDamaj@eurofins.com>
2023-06-23 10:11:13 +0200

    changed

--- foo/bar/Program.cs
+++ foo/bar/Program.cs
@@ -3,10 +3,6 @@
 var repoName = "Test";
 var rootDirectory = "C:\\Git Test\\test";
 var branchName = "development";
-<<<<<<< HEAD
 var sourcePath = "Foo/changed";
-=======
-var sourcePath = "Foo/test";
->>>>>>> 92c87cacf161775a3ae8bcafe1c84eec5aa2297d
 
 
